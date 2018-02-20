bbb939f2cf9789ffbf6382fa83a75b505160f8a3
Olivie <34624398+OlivierrCP@users.noreply.github.com>
2018-02-20 15:55:08 +0100

    asd
    asd

--- bot.js
+++ bot.js
@@ -6,19 +6,6 @@ const fs = require('fs');
 
 const client = new Discord.Client();
 
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
-
-
 const config = require("./config.json");
 
 const folder = './src/commands/pictures/'; //Define pictures folder
@@ -100,4 +87,4 @@ client.on("message", async message => {
 
 
 
-  client.login(config.token);
+client.login(process.env.BOT_TOKEN);
